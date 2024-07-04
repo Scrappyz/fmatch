dbc3e9eea3b036a44ebb0b6a2179525532adcd79
Scrappyz <ghostmic3000@gmail.com>
2024-07-04 21:59:08 +0800

    added asterisk

--- include/fnmatch.hpp
+++ include/fnmatch.hpp
@@ -1,2 +1,41 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+namespace fnmatch {
+
+    bool match(const std::string& pattern, const std::string& str)
+    {
+        int i = 0;
+        int j = 0;
+        while(i < pattern.size() && j < str.size()) {
+            if(pattern[i] == '*') {
+                i++;
+
+                if(i >= pattern.size()) {
+                    return true;
+                }
+
+                while(j < str.size() && pattern[i] != str[j]) {
+                    j++;
+                }
+
+                if(j >= str.size()) {
+                    return false;
+                }
+            } else if(pattern[i] != str[j]) {
+                return false;
+            } else {
+                i++;
+                j++;
+            }
+        }
+
+        if(i < pattern.size() || j < str.size()) {
+            return false;
+        }
+
+        return true;
+    }
+}
--- test/src/fnmatch_test.cpp
+++ test/src/fnmatch_test.cpp
@@ -1,8 +1,29 @@
 #include <gtest/gtest.h>
+#include "fnmatch.hpp"
 
 using namespace std;
 
-TEST(MyTest, play)
+TEST(match, matching)
 {
-    EXPECT_EQ(true, true);
+    EXPECT_EQ(fnmatch::match("potato/normal/go", "potato/normal/go"), true);
+    EXPECT_EQ(fnmatch::match("potato/normal/go", "potato/normal/god"), false);
+    EXPECT_EQ(fnmatch::match("potato/normal/god", "potato/normal/go"), false);
+    EXPECT_EQ(fnmatch::match("potato/nl/god", "potato/normal/go"), false);
+}
+
+TEST(match, asterisk)
+{
+    EXPECT_EQ(fnmatch::match("potato/n*l/go", "potato/normal/go"), true);
+    EXPECT_EQ(fnmatch::match("po*o/normal/go", "potato/normal/go"), true);
+    EXPECT_EQ(fnmatch::match("my_project/*.cpp", "my_project/main.cpp"), true);
+}
+
+TEST(match, asterisk_end)
+{
+    EXPECT_EQ(fnmatch::match("potato/normal/go*", "potato/normal/godude"), true);
+}
+
+TEST(match, multiple_asterisk)
+{
+    EXPECT_EQ(fnmatch::match("po*o/n*l/go*", "potato/normal/godude"), true);
 }
